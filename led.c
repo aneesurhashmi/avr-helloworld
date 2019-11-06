304e82d0a458dc46ae60bbb577546eb47c308b92
Anees Hashmi <aneesurhashmi@gmail.com>
2019-11-06 17:28:28 +0500

    led blink

---
+++ led.c
@@ -0,0 +1,14 @@
+#define F_CPU 16000000L 
+#include <avr/io.h>
+#include <util/delay.h>
+ 
+int main(void) {
+  DDRB |= 1<<5;
+  while(1){
+    PORTB |= 1<<5;
+    _delay_ms (1000);
+    PORTB &= ~(1<<5);
+    _delay_ms (1000);
+  }
+  return 0;
+}
